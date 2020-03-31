a0eb8790843150a494ccf4636932fb541916ad51
Thomas BENARD <thomas0benard@gmail.com>
2020-04-01 00:11:54 +0200

    added Quotecard.js & CSS

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-//import logo from './logo.svg';
-//import './App.css';
+import QuoteCard from './components/QuoteCard';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <QuoteCard />
     </div>
   );
 }
 
 export default App;
 
-/*
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-*/
