5dd68e7306abd6d637c9e3022992ec1781a7de38
Thomas BENARD <thomas0benard@gmail.com>
2020-03-31 22:56:05 +0200

    added Navbar.js & CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 //import logo from './logo.svg';
 //import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <nav class="Navbar"><a href="https://fr.wikipedia.org/wiki/Les_Simpson" target="_blank">Simpsons' Wiki</a></nav>
+    );
+}
+
+export default Navbar;
