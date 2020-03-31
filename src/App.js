140f24aba406105a9fdf080ee5690c616ddfe385
Thomas BENARD <thomas0benard@gmail.com>
2020-03-31 22:33:47 +0200

    removed div content in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
 function App() {
   return (
     <div className="App">
+    </div>
+  );
+}
+
+export default App;
+
+/*
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -19,8 +27,4 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
-
-export default App;
+*/
