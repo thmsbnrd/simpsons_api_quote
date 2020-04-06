1fdef1c4fd193a09758d60e330d472169a773747
Thomas BENARD <thomas0benard@gmail.com>
2020-04-06 13:56:55 +0200

    first commit all

--- src/App.js
+++ src/App.js
@@ -6,8 +6,16 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <QuoteCard />
-      <QuoteCard />
+      <QuoteCard 
+        quote="Ahh! Sweet liquor eases the pain."
+        character="Troy McClure"
+        image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FTroyMcClure.png?1497567511112" 
+      />
+      <QuoteCard 
+        quote="Eat my shorts"
+        character="Bart Simpson"
+        image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FBartSimpson.png?1497567511638"
+      />
     </div>
   );
 }
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './QuoteCard.css';
 
-function QuoteCard () {
+function QuoteCard (props) {
     return (
      <figure class = "QuoteCard">
-         <img src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
-      alt="Nelson Muntz" />
+         <img src={props.image}
+      alt={props.character} />
       <figcaption>
           <blockquote>
-          Shoplifting is a victimless crime, like punching someone in the dark.
+            {props.quote}
           </blockquote>
-          <cite>Nelson Muntz</cite>
+          <cite>{props.character}</cite>
       </figcaption>
      </figure>
     );
