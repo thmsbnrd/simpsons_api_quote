1fb1b86c2b74098437472587db4cf705dda0f385
Thomas BENARD <thomas0benard@gmail.com>
2020-04-13 15:04:53 +0200

    added props to QuoteCard.js

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <QuoteCard 
-        quote="Ahh! Sweet liquor eases the pain."
-        character="Troy McClure"
-        image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FTroyMcClure.png?1497567511112" 
+      <QuoteCard
+        quote="I believe the children are the future... Unless we stop them now!"
+        characterFirstName="Homer"
+        characterLastName="Simpson"
+        image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+      />
+      <QuoteCard
+        quote="Me fail English? That's unpossible"
+        characterFirstName="Ralph"
+        characterLastName="Wiggum"
+        image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
       />
       <QuoteCard 
-        quote="Eat my shorts"
-        character="Bart Simpson"
-        image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FBartSimpson.png?1497567511638"
+        quote="Shoplifting is a victimless crime, like punching someone in the dark." 
       />
     </div>
   );
 }
-
 export default App;
-
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './QuoteCard.css';
 
-function QuoteCard (props) {
-    return (
-     <figure class = "QuoteCard">
-         <img src={props.image}
-      alt={props.character} />
+function QuoteCard(props) {
+  return (
+    <figure className="QuoteCard">
+      <img src={props.image} alt={props.character} />
       <figcaption>
-          <blockquote>
-            {props.quote}
-          </blockquote>
-          <cite>{props.character}</cite>
+        <blockquote>{props.quote}</blockquote>
+        <cite>{props.characterFirstName + ' ' + props.characterLastName}</cite>
       </figcaption>
-     </figure>
-    );
+    </figure>
+  );
 }
+
+QuoteCard.propTypes = {
+  characterFirstName: PropTypes.string.isRequired,
+  characterLastName: PropTypes.string.isRequired,
+};
 
 export default QuoteCard;
