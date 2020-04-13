1566f37d4dd56b5b58ce02dacadd6eb818007925
Thomas BENARD <thomas0benard@gmail.com>
2020-04-13 15:20:18 +0200

    created QuoteList.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import QuoteCard from './components/QuoteCard';
+//import QuoteCard from './components/QuoteCard';
+import QuoteList from './components/QuoteList';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <QuoteCard
-        quote="I believe the children are the future... Unless we stop them now!"
-        characterFirstName="Homer"
-        characterLastName="Simpson"
-        image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
-      />
-      <QuoteCard
-        quote="Me fail English? That's unpossible"
-        characterFirstName="Ralph"
-        characterLastName="Wiggum"
-        image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
-      />
-      <QuoteCard 
-        quote="Shoplifting is a victimless crime, like punching someone in the dark." 
-      />
+      <QuoteList />
     </div>
   );
 }
