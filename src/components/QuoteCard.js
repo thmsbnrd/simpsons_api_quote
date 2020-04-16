84fe400a6c5a018b2f33aae7269d8e0d1a3c6254
Thomas BENARD <thomas0benard@gmail.com>
2020-04-16 17:07:01 +0200

    added state favorite to QuoteCard

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -3,13 +3,27 @@ import React from 'react';
 import './QuoteCard.css';
 
 class QuoteCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { favorite: false}
+  }
+
   render() {
     return (
       <figure className="QuoteCard">
         <img src={this.props.image} alt={this.props.character} />
         <figcaption>
           <blockquote>{this.props.quote}</blockquote>
-          <cite>{this.props.character}</cite>
+          <p>
+            <cite>{this.props.character}</cite>
+            <span 
+            onClick={event => {
+              const newFav = !this.state.favorite;
+              this.setState({ favorite: newFav });
+            }}
+            className={this.state.favorite ? "is-favorite" : ""}
+            >&#9733;</span>
+          </p>
         </figcaption>
       </figure>
     );
