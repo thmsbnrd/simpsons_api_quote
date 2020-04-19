3d26d1ae233ff97f78ff674956d373f787d5fdeb
Thomas BENARD <thomas0benard@gmail.com>
2020-04-19 11:12:52 +0200

    imported QuoteForm to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import QuoteList from './components/QuoteList';
+import QuoteForm from './components/QuoteForm';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <QuoteForm />
       <QuoteList />
     </div>
   );
